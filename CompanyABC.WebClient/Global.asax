354d4cf0d9e2e3ed3915ab598176d5862ca59219
Craig McLellen <craig.mclellen@showzapp.com.au>
2014-11-13 20:42:42 +1100

    Add initial project files.

---
+++ CompanyABC.WebClient/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CompanyABC.WebClient.MvcApplication" Language="C#" %>
---
+++ _shared/GlobalAssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CompanyABC")]
+[assembly: AssemblyProduct("LeaveRequestWebApp")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.*")]
